4c268e19b6d1e9671aed83bc1af2f35377846ef3
ZUD0 <67162838+ZUD0@users.noreply.github.com>
2021-07-21 11:16:07 +0530

    Delete test2.c

--- test2.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-#include<string.h>
-int main(void){
-    char name[20];
-    printf("Enter the name");
-    gets(name);
-    printf("The size of the name is %d", strlen(name));
-}
